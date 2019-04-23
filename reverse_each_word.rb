ec357123e9336e9df155ca9775f5ae64acd8b55e
James Hayward <james.hayward6@icloud.com>
2019-04-23 10:21:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,18 +13,12 @@ require "pry"
 # return r_string
 # end
 
-# def reverse_each_word(sentence)
-#   s_array = sentence.reverse.split
-#   r_array = []
-#   s_array.reverse.collect do |word| #for collect, you should use a . here, but for each you should use an _
-#     r_array.push(word)
-#   end
-#   r_string = r_array.join(" ")
-# return r_string
-# end
-
 def reverse_each_word(sentence)
   s_array = sentence.reverse.split
-  s_array.reverse.collect
-  return s_array.join(" ")
+  r_array = []
+  s_array.reverse.collect do |word| #for collect, you should use a . here, but for each you should use an _
+    r_array.push(word)
+  end
+  r_string = r_array.join(" ")
+return r_string
 end
